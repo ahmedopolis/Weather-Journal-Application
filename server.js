8c7c892f48e23c07d7d989515b8dd427eb32f092
aimogue <ahmed.ibrahimmogue@gmail.com>
2021-01-10 19:19:51 -0500

    ⬆️ Dependencies: Add node and other modules

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 // Setup empty JS object to act as endpoint for all routes
+let websiteData = {};
+
 // Express to run server and routes
+const express = require("express");
 
 // Start up an instance of app
+const app = express();
 
-/* Dependencies */
-/* Middleware*/
+/* Dependencies & Middleware */
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 //Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
+app.use(express.static("website"));
 
 // Spin up the server
+const port = 8000;
+const server = app.listen(port, listening);
+function listening() {
+  console.log(server);
+  console.log(`running on localhost: ${port}`);
+}
+
 // Callback to debug
 
 // Initialize all route with a callback function
