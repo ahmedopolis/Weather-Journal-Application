08959b7cf7153e282e93bffcd748385c4626740f
aimogue <ahmed.ibrahimmogue@gmail.com>
2021-01-10 20:56:07 -0500

    🔧 Refactor: Optimize Api key access route

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
+// Load-in env variables
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+/**
+ *  Api key is accessed via .env file and this key is accessed
+ * in server.js to avoid exposure via 'devTools'.
+ * */
+const OpenWeatherMapApiKey = process.env.OpenWeatherMapApiKey;
+
 // Setup empty JS object to act as endpoint for all routes
-let websiteData = {};
+let projectData = {};
 
 // Express to run server and routes
 const express = require("express");
@@ -30,9 +41,23 @@ function listening() {
 }
 
 // Callback to debug
+function debugCodebase() {
+  debugger;
+}
 
 // Initialize all route with a callback function
+app.get("/all", sendData);
 
 // Callback function to complete GET '/all'
+function sendData(req, res) {
+  res.send(projectData);
+}
 
 // Post Route
+const data = [];
+
+app.post("/weather", addWeather);
+
+function addWeather(req, res) {
+  data.push(req.body);
+}
